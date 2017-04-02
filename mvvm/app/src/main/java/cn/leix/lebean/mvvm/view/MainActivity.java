eee5f2d1c958576567e0b1792f6744e27db69d71
lebean <xleigs@163.com>
2017-04-02 17:10:08 +0800

    init app

---
+++ mvvm/app/src/main/java/cn/leix/lebean/mvvm/view/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.leix.lebean.mvvm.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.leix.lebean.mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
